5e6c7ff571427362a30549712462e8aa94af7a74
gajendra <davegk59@gmil.com>
2024-08-05 17:06:54 +0530

    activated  form

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 //alert('hello'); 
-function addNewField() {
-    let newNode = document.getElementById('textarea');
+function addNewWEField() {
+    let newNode = document.createElement('textarea');
+    newNode.classList.add('form-control');
+    newNode.classList.add('weField');
+    newNode.classList.add('mt-2');
+    newNode.setAttribute("row", 3);
+    newNode.setAttribute('placeholder', "Enter working field")
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+    weOb.insertBefore(newNode, weAddButtonOb);
     
+}
+
+function addNewAQField() {
+    let newNode = document.createElement('textarea');
+    newNode.classList.add('form-control');
+    newNode.classList.add('aqField');
+    newNode.classList.add('mt-2');
+    newNode.setAttribute('rows', 3);
+    newNode.setAttribute('placeholder', "Enter Qualifications");
+
+    let aqob = document.getElementById('aq');
+    let aqAddButtonOb = document.getElementById('aqAddButton');
+    aqob.insertBefore(newNode,aqAddButtonOb);
 }
